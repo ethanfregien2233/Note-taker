63f9f89ad7b1d993c866331b3368df4126eccc34
ethan fregien <ethanfregien@gmail.com>
2021-12-13 18:56:54 -0600

    setting up express

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,23 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+fs.readFile("./db/db.json","json",function(data) {
+    var notes = data;
+});
+
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});   
+
 let noteTitle;
 let noteText;
 let saveNoteBtn;
