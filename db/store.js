e200fb588d167c55fd6c5e8d3534fbc4ae08738d
ethan fregien <ethanfregien@gmail.com>
2021-12-17 11:57:41 -0600

    code cleanup

---
+++ db/store.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const path = require("path");
+const util = require("util");
+const express = require("express");
+const app = express();
+
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+    constructor() {
+        this.lastId = 0;
+    };
+    read() {
+        return readFileAsync(path.join(__dirname, "db.json"), "utf8");
+    };
+    write(note) {
+        return writeFileAsync(path.join(__dirname, "db.json"), JSON.stringify(note));
+    };
+    getNotes() {
+        return this.read().then(notes => {
+            let parsedNotes = JSON.parse(notes);
+            console.log(parsedNotes);
+            return parsedNotes;
+        });
+    };
+    addNote(newNote) {
+        console.log(newNote);
+        return this.getNotes().then(notes => {
+            const newNoteList = [...notes, newNote]; 
+            console.log(newNoteList);
+            return this.write(newNoteList);
+        })
+    
+    };
+    deleteNotes(title) {
+        return this.getNotes()
+            .then(notes => {
+                console.log("This note says " + title);
+                for (var i = 0; i < notes.length; i++) {
+                    if (notes[i].title === title) {
+                        notes.splice(i, 1);
+                        console.log(notes);
+                        break;
+                    }
+                }
+                this.write(notes);
+            })
+    }
+};
+
+const store = new Store();
+
+module.exports = store;
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+
+const path = require("path");
+const router = require("express").Router();
+
+router.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+// If no matching route is found default to home
+router.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = router;
